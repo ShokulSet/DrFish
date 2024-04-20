33357154cfc75d89e58221224f02495523c084a8
ShokulSet <Settapun.work@gmail.com>
2024-04-20 17:49:48 +0700

    Add a somewhat funtional camera button

--- GlooGlooApp/src/Camera.tsx
+++ GlooGlooApp/src/Camera.tsx
@@ -1,10 +1,19 @@
 import { useEffect, useRef } from 'react';
-import { useCameraDevice, Camera, useCameraPermission,} from 'react-native-vision-camera';
+import { useCameraDevice, Camera, useCameraPermission, TakePhotoOptions} from 'react-native-vision-camera';
 import { ActivityIndicator, Pressable, StyleSheet, Text, View } from 'react-native';
 
 function CameraScreen() {
     const { hasPermission, requestPermission } = useCameraPermission()
     const device = useCameraDevice('back')
+    const camera = useRef<Camera>(null)
+    
+    const onTakePicturePressed = async () => {
+        const photo = await camera.current?.takePhoto({
+          enableShutterSound: false
+        });
+        console.log(photo)
+        // Magic AI Work then pokedex
+    }
 
     useEffect(() => {
       if (!hasPermission) {
@@ -23,12 +32,28 @@ function CameraScreen() {
     return (
       <View style={{ flex: 1}}>
         <Camera
+        ref={camera}
         style={StyleSheet.absoluteFill}
         device={device}
         isActive={true}
+        photo={true}
         /> 
+        <Pressable 
+            onPress={onTakePicturePressed}
+            style={{
+                position: 'absolute',
+                alignSelf: "center", 
+                bottom: 50, 
+                width: 70, 
+                height: 70, 
+                backgroundColor: 'white',
+                borderRadius: 75
+            }} 
+        />
       </View>
     )
 }
+
+const styles = StyleSheet.create
 
 export default CameraScreen;
