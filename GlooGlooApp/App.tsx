a5c0bfedf9c6730bc07983f1c109226dd14ae36d
ShokulSet <Settapun.work@gmail.com>
2024-05-10 02:30:42 +0700

    Added modal and new button animation and hide status bar

--- GlooGlooApp/App.tsx
+++ GlooGlooApp/App.tsx
@@ -1,5 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StatusBar } from 'react-native';
 import CameraScreen from './src/Camera.tsx'
 import PokedexScreen from './src/Pokedex.tsx'
 
@@ -7,8 +8,9 @@ const Tab = createBottomTabNavigator();
 
 function App() {
   return (
-    // navigation bar at the  buttom 
+    // navigation bar at the  buttom
     <NavigationContainer>
+      <StatusBar hidden />
       <Tab.Navigator>
       <Tab.Screen name="Cam" component={CameraScreen} />
       <Tab.Screen name="Dex" component={PokedexScreen} />
--- GlooGlooApp/src/Camera.tsx
+++ GlooGlooApp/src/Camera.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useRef, useState } from 'react';
 import { useCameraDevice, Camera, useCameraPermission, PhotoFile } from 'react-native-vision-camera';
-import { ActivityIndicator, Pressable, StyleSheet, Text, View, Image } from 'react-native';
+import { ActivityIndicator, Pressable, StyleSheet, Text, View, Modal, } from 'react-native';
+
 
 function CameraScreen() {
     const { hasPermission, requestPermission } = useCameraPermission()
     const device = useCameraDevice('back')
     const [photo, setPhoto] = useState<PhotoFile>();
+    const [modalVisible, setModalVisible] = useState(false);
     const camera = useRef<Camera>(null)
     
     const onTakePicturePressed = async () => {
@@ -16,6 +18,7 @@ function CameraScreen() {
         });
         console.log(photo)
         // Magic AI Work then pokedex
+        setModalVisible(true)
     }
 
     useEffect(() => {
@@ -33,8 +36,8 @@ function CameraScreen() {
     }
 
     return (
-      <View style={{ flex: 1}}>
-
+      
+      <View style={styles.centeredView}>
         <Camera
         ref={camera}
         style={StyleSheet.absoluteFill}
@@ -45,20 +48,60 @@ function CameraScreen() {
 
         <Pressable 
             onPress={onTakePicturePressed}
-            style={{
-                position: 'absolute',
-                alignSelf: "center", 
-                bottom: 50, 
-                width: 70, 
-                height: 70, 
-                backgroundColor: 'white',
-                borderRadius: 75
-            }} 
+            style={({pressed}) => [
+              {
+                backgroundColor: pressed ? 'rgb(210, 230, 255)' : 'white',
+              },
+              styles.buttonContainer
+            ]}
         />
+
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            console.log("load Closed")
+            setModalVisible(!modalVisible);
+          }}>
+          <View style={styles.centeredView}>
+            <View style={styles.loadView}>
+              <Text>Modal testing</Text>
+            </View>
+          </View>
+        </Modal>
+
       </View>
     )
 }
 
-const styles = StyleSheet.create
+const styles = StyleSheet.create({
+  buttonContainer : {
+    position: 'absolute',
+    alignSelf: "center", 
+    bottom: 50, 
+    width: 70, 
+    height: 70, 
+    borderRadius: 75
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  loadView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    }
+  }
+})
 
 export default CameraScreen;
