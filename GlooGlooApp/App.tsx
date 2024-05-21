c72be84b037ee425c8dd974a1a7a29a8bf874f17
ShokulSet <Settapun.work@gmail.com>
2024-05-22 01:42:15 +0700

    bootsplash change

--- GlooGlooApp/App.tsx
+++ GlooGlooApp/App.tsx
@@ -1,12 +1,27 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StatusBar } from 'react-native';
+import { useEffect } from 'react';
+import BootSplash from "react-native-bootsplash";
 import CameraScreen from './src/Camera.tsx'
 import PokedexScreen from './src/Pokedex.tsx'
 
 const Tab = createBottomTabNavigator();
 
+function delay(ms: number) {
+  return new Promise( resolve => setTimeout(resolve, ms) );
+}
+
 function App() {
+  useEffect(() => {
+    const init = async () => {
+    // loading process
+    };
+    init().finally(async () => {
+      await BootSplash.hide({ fade: true });
+      console.log("BootSplash has been hidden successfully");
+    });
+  }, []);
   return (
     // navigation bar at the  buttom
     <NavigationContainer>
--- GlooGlooApp/android/app/src/main/java/com/glooglooapp/MainActivity.kt
+++ GlooGlooApp/android/app/src/main/java/com/glooglooapp/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.glooglooapp
 
+import android.os.Bundle
+import com.zoontek.rnbootsplash.RNBootSplash
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -19,4 +21,9 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    RNBootSplash.init(this, R.style.BootTheme) // ⬅️ initialize the splash screen
+    super.onCreate(savedInstanceState) // super.onCreate(null) with react-native-screens
+  }
 }
